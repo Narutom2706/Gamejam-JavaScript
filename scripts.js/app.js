b480b51932dc535143309ed45654649b4ebe93f1
sofianecharifi <sofiane.charifialaoui@gmail.com>
2026-01-23 16:59:15 +0100

    add initial HTML structure and game data handling

---
+++ scripts.js/app.js
@@ -0,0 +1,6 @@
+// il faut enregistrer les inputs du joueurs et refaire systématiquement les mêmes 
+// ou bien enregistrer les coordonnées 
+
+// Structure de base d'un enregistrement
+const ghostData = []; 
+// Chaque entrée ressemblera à : { x: 100, y: 200, spriteState: 'jump', faceRight: true }
